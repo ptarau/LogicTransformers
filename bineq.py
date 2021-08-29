19df499b8ca318a81d157cb569a1e7895cd9994a
Paul Tarau <ptarau@gmail.com>
2021-08-29 10:41:50 -0500

    progress on bineq

--- bineq.py
+++ bineq.py
@@ -5,12 +5,12 @@ DEB = True
 def load(fname='out/bineq_asm.txt'):
     def to_const(x):
         try:
-            return int(x), INT
+            return tag(int(x), INT)
         except ValueError:
             try:
-                return float(x), FLOAT
+                return tag(float(x), FLOAT)
             except ValueError:
-                return atom(x)
+                return tag(x,CONST)
 
     with open(fname, 'r') as f:
         txt = f.read()
@@ -27,11 +27,11 @@ def load(fname='out/bineq_asm.txt'):
                 if x in vs:
                     v = vs[x]
                 else:
-                    v = var(len(vs))  # , x
+                    v = tag(len(vs),VAR)  # , x
                     vs[x] = v
                 xs[j] = v
             elif x[0] == "_":
-                v = var(len(vs)),  # x
+                v = tag(len(vs),VAR),  # x
                 vs[x] = v
                 xs[j] = v
             elif x == '[|]':
@@ -71,89 +71,74 @@ else:
     READ, WRITE = 1, 2
 
 
-def arity(n):
-    assert isinstance(n, int)
-    return (n, ARITY)
+def tag_of(x):
+    return x[1]
 
 
-def atom(s):
-    assert isinstance(s, str)
-    return sym(s), CONST
-
-def var(x):
-    assert x is None or isinstance(x, int)
-    return (x, VAR)
-
-def arg(i):
-    assert isinstance(i, int)
-    return i,INT
+def val(x):
+    return x[0]
 
 
-def put_fun(n):
-    n = as_val(n)
+def tag(x, t):
+    return (x, t)
+
+
+def vget(h):
+    assert isinstance(h, int)
+    x = heap[h]
+    assert isinstance(x, tuple)
+    return x
+
+
+def vset(h, x):
+    assert isinstance(h, int)
+    assert isinstance(x, tuple)
+    heap[h] = x
+
+
+def put_size(arity):
     h = len(heap)
-    heap.append(arity(n[0]))
-    heap.extend([var(None)] * n[0])
-    return var(h)
+    n = val(arity)
+    heap.append(arity)
+    heap.extend([tag(None, VAR)] * n)
+    return tag(h, VAR)
 
 
-def get_fun(n_, v):
-    n, _ = as_val(n_)
-    v = as_var(v)
-    r, t = get_val(v)
-    assert t == ARITY
-    return n == r
+def get_size(arity, v):
+    n = val(arity)
+    m = val(v)
+    t = tag_of(v)
+    return ARITY == t and n == m
 
 
 def put_arg(i_, h_, x):
-    i, ti = as_val(i_)
-    h, th = as_var(h_)
+    i = val(i_)
+    ti = tag_of(i_)
+    h = val(h_)
+    th = tag_of(h_)
+    assert INT == ti
+    assert VAR == th
     hi = h + i
-    heap[hi] = x
-    return var(hi)
+    vset(hi, x)
 
 
 def unify_arg(i_, h_, x, trail, htop):
-    i, ti = as_val(i_)
-    h, th = as_var(h_)
-    assert VAR == ti
+    i = val(i_)
+    ti = tag_of(i_)
+    h = val(h_)
+    th = tag_of(h_)
+    assert INT == ti
     assert VAR == th
-    y = heap[h + i]
+    hi = h + i
+    y = vget(hi)
     return unify(x, y, trail, htop)
-
-
-def as_val(v):
-    print('@@@',v)
-    assert isinstance(v, tuple)
-    assert len(v) == 2
-    assert isinstance(v[0], int) or isinstance(v[0], str)
-    return v
-
-
-def as_var(v):
-    assert isinstance(v, tuple)
-    assert len(v) == 2
-    assert isinstance(v[0], int)
-    return v
-
-
-def get_val(v):
-    v = as_val(v)
-    r = heap[v[0]]
-    return as_val(r)
-
-
-def set_val(v, t):
-    v = as_val(x)
-    t = as_val(t)
-    heap[v[0]] = t
 
 
 def deref(o):
     while True:
-        x, t = o
-        if t == VAR:
-            v, tv = get_val(x)
+        x, tx = o
+        if tx == VAR:
+            v, tv = vget(x)
             if v is None:
                 assert VAR == tv
                 return o
@@ -161,7 +146,6 @@ def deref(o):
                 print('DEREF:', v, '>', x)
                 assert v < x
             o = v, tv
-
         else:
             return o
 
@@ -178,16 +162,16 @@ def unify(x, y, trail, htop):
             pass
         elif VAR == t1 and VAR == t2:
             if x1 > x2:
-                set_val(x1, (x2, t2))
+                vset(x1, (x2, t2))
                 if x1 < htop: trail.append(x1)
             else:
-                set_val(x2, (x1, t1))
+                vset(x2, (x1, t1))
                 if x2 < htop: trail.append(x2)
         elif VAR == t1:
-            set_val(x1, (x2, t2))
+            vset(x1, (x2, t2))
             if x1 < htop: trail.append(x1)
         elif VAR == t2:
-            set_val(x2, (x1, t1))
+            vset(x2, (x1, t1))
             if x2 < htop: trail.append(x2)
         elif t1 != t2:
             return False
@@ -200,8 +184,8 @@ def unify(x, y, trail, htop):
             if x1 != x2:
                 return False
             for i in range(x1 + 1, 0, -1):
-                a = get_val(x1 + i)
-                b = get_val(x2 + i)
+                a = vget(x1 + i)
+                b = vget(x2 + i)
                 ustack.append(b)
                 ustack.append(a)
     return True
@@ -210,15 +194,14 @@ def unify(x, y, trail, htop):
 def unwind(trail, ttop):
     for _ in range(len(trail) - ttop):
         v = trail.pop()
-        set_val(v, (v, VAR))
+        vset(v, (v, VAR))
 
 
 def trim_heap(htop):
     # return
-    l = len(heap)
-    for _ in range(htop, l):
+    hl = len(heap)
+    for _ in range(htop, hl):
         heap.pop()
-
 
 
 FAIL, DO, DONE, UNDO = 0, 1, 2, 3
@@ -233,32 +216,16 @@ def step(G, code, trail, goal, i):
     while i < l:
         unwind(trail, ttop)
         trim_heap(htop)
-        clause = activate(G, code[i], trail, ttop, htop)
+        clause = code[i]
         i += 1
-        if clause:
-            hb, thb = clause
-            assert PAIR == thb
-            H = left(hb)
-            assert H[0] >= htop
-            assert G[0] < htop
-            unify(G, H, trail, htop)
-            b, tb = deref(right(hb))
-            if VAR == tb:
-                # yield p(left(goal[0]))
-                # print("GOAL",iron(goal))
-                print(p(left(goal[0])))
-                return (DONE, G, ttop, htop, i)
-            else:
-                NewG = b, tb
-                assert b < len(heap)
-                # yield from loop(NewG,k+1)
-                return (DO, (NewG, G), ttop, htop, i)
+        print(clause)
     return (FAIL, None, ttop, htop, i)
 
+
 def get_goal():
-    h=put_fun(arity(2))
-    put_arg(arg(1),h,atom('goal'))
-    put_arg(arg(2),h,var(None))
+    h = put_size(tag(2,ARITY))
+    put_arg(tag(1,INT), h, tag('goal',CONST))
+    put_arg(tag(2,INT), h, tag(None,VAR))
     return h
 
 
@@ -275,8 +242,8 @@ def interp():
         op, G, ttop, htop, i = todo.pop()
         if DO == op:
             (NewG, OldG) = G
-            print("$$$$ G=",G)
-            if i < l: ensure_undo(OldG, todo, ttop, htop, i, l);
+            print("$$$$ G=", G)
+            if i < l: ensure_undo(OldG, todo, ttop, htop, i, l)
             todo.append(step(NewG, code, trail, goal, 0))
 
         elif DONE == op:
@@ -305,7 +272,7 @@ def go():
     interp()
 
 
-####### tests #####
+# tests
 
 def ltest():
     print('testing')
@@ -317,6 +284,7 @@ def ltest():
             print(x)
     print('syms')
     print(syms)
+    print('clauses:', len(code))
 
 
 if __name__ == "__main__":
