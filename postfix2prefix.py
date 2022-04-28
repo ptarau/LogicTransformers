cecc9b16f20c16cb17a22c2bb36f79d489a632cf
Paul Tarau <ptarau@gmail.com>
2022-04-28 17:53:29 -0500

    minor

--- postfix2prefix.py
+++ postfix2prefix.py
@@ -31,9 +31,27 @@ def from_postfix(ws):
     return stack.pop()
 
 
+def to_heap(term):
+    args = [term]
+    stack = []
+    while args:
+        t = args.pop()
+        if not isinstance(t, tuple):
+            stack.append(t)
+        else:
+            stack.append(arity(len(t)))
+            for x in reversed(t):
+                args.append(x)
+    return list(stack)
+
+
+
 t = (1, ((2,), 3, 3.14), (4, 5), (6, (7, 8)), 9, 'ten')
 print(t)
 xs = to_postfix(t)
 print(xs)
 tt = from_postfix(xs)
+print()
 print(tt)
+ys = to_heap(t)
+print(ys)
